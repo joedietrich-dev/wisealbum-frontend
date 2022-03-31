de7aef9f52765a119cdf361eda11037e2ee13fe3
joedietrich-dev <joe@joedietrich.net>
2022-03-30 21:13:28 -0400

    ibid

---
+++ src/components/Deck.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components/macro";
+
+const Deck = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 16px;
+`;
+
+export default Deck;
