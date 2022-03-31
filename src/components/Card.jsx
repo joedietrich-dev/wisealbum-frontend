8b1f9f54d2d3085cbced1023a7e69612c1b376a7
joedietrich-dev <joe@joedietrich.net>
2022-03-30 21:13:21 -0400

    Stub Card Styles

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components/macro";
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  box-shadow: 0.2px 0.2px 0.5px rgba(0, 0, 0, 0.035), 0.6px 0.6px 1.3px rgba(0, 0, 0, 0.05), 1.5px 1.5px 3px rgba(0, 0, 0, 0.065),
+    5px 5px 10px rgba(0, 0, 0, 0.1);
+`;
+export default Card;
---
+++ src/components/CardTitle.jsx
@@ -0,0 +1,5 @@
+import styled from "styled-components/macro";
+
+const CardTitle = styled.h3``;
+
+export default CardTitle;
