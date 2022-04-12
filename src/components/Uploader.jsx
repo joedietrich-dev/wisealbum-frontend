3825cdde72eb349d46298526eb8c79f4096cdc19
joedietrich-dev <joe@joedietrich.net>
2022-04-11 20:52:52 -0400

    Basic uploading indicator

--- src/components/Uploader.jsx
+++ src/components/Uploader.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { authorizedPost } from "../helpers/fetchers/post";
 import UploadArea from "./UploadArea";
@@ -15,28 +15,40 @@ async function getSignedUrl(filename) {
 async function uploadDirectly(
   file,
   filePath = "",
+  handleFileSet = (f) => f,
   onUpload = (file, url) => {
     return;
   }
 ) {
   const filename = filePath ? `${filePath}${file.name}` : file.name;
-  console.log(filePath, filename);
+
+  handleFileSet((fileStatuses) => [...fileStatuses, { fileName: file.name, status: "uploading" }]);
+
   const signedUrl = await getSignedUrl(filename);
   const res = await fetch(signedUrl, {
     method: "PUT",
     body: file,
   });
   if (res.ok) {
+    handleFileSet((fileStatuses) =>
+      fileStatuses.map((fileStatus) => (fileStatus.fileName === file.name ? { ...fileStatus, status: "complete" } : fileStatus))
+    );
     onUpload(file, `${process.env.REACT_APP_MEDIA_DOMAIN}/${filename}`);
     return res.url;
   }
+  handleFileSet((fileStatuses) =>
+    fileStatuses.map((fileStatus) => (fileStatus.fileName === file.name ? { ...fileStatus, status: "error" } : fileStatus))
+  );
 }
 
 function Uploader({ filePath = "", onUpload = (f) => f }) {
+  const [fileStatuses, setFileStatuses] = useState([]);
+
   const onDrop = useCallback(
     (files) => {
+      setFileStatuses((fileStatuses) => fileStatuses.filter((fileStatus) => fileStatus.status === "complete"));
       files.forEach((file) => {
-        uploadDirectly(file, filePath, onUpload);
+        uploadDirectly(file, filePath, setFileStatuses, onUpload);
       });
     },
     [filePath, onUpload]
@@ -44,10 +56,20 @@ function Uploader({ filePath = "", onUpload = (f) => f }) {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <UploadArea {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? <p>Drop the files here ...</p> : <p>Drag 'n' drop some files here, or click to select files</p>}
-    </UploadArea>
+    <>
+      <UploadArea {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? <p>Drop the files here ...</p> : <p>Drag 'n' drop some files here, or click to select files</p>}
+      </UploadArea>
+      {fileStatuses.map(
+        (fileStatus) =>
+          fileStatus.status !== "complete" && (
+            <div>
+              {fileStatus.fileName}: {fileStatus.status}
+            </div>
+          )
+      )}
+    </>
   );
 }
 
