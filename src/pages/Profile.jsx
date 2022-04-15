f18fd21d7baa48928c59dc5b72ea702bd5924ebc
joedietrich-dev <joe@joedietrich.net>
2022-04-14 22:47:10 -0400

    Edit Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,46 @@
 import PageCard from "../components/PageCard";
 import Title from "../components/Title";
+import SectionTitle from "../components/SectionTitle";
+import Subtitle from "../components/Subtitle";
 import { useAuth } from "../helpers/AuthorizationProvider";
+import { post } from "../helpers/fetchers/post";
+import { useState } from "react";
+import { Form, Formik } from "formik";
+import TextInput from "../components/TextInput";
 
 function Profile() {
   const { user, loading } = useAuth();
+  const [isResetSent, setIsResetSent] = useState(false);
+  const handleSubmit = (data) => {
+    console.log(data, user);
+    post("/password", data);
+    setIsResetSent(true);
+  };
   return (
     <>
       {loading ? (
         <PageCard>Loading...</PageCard>
       ) : (
         <PageCard>
-          <Title>Edit {user.full_name}</Title>
+          <Title>{user.full_name} || Profile</Title>
+          <Subtitle>You are a member of the organization with id {user.organization_id}!</Subtitle>
+          <SectionTitle>Password Reset</SectionTitle>
+          {isResetSent ? (
+            <div>Instructions have been sent to your address.</div>
+          ) : (
+            <Formik
+              initialValues={{
+                email: "",
+              }}
+              onSubmit={handleSubmit}
+            >
+              <Form>
+                <Subtitle>Enter your email address to reset your password</Subtitle>
+                <TextInput label="Email Address" name="email" type="email" />
+                <button type="submit">Submit</button>
+              </Form>
+            </Formik>
+          )}
         </PageCard>
       )}
     </>
