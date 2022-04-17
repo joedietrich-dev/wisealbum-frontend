d2d25ffd1624f77f3fc0e655d2162136c549287d
joedietrich-dev <joe@joedietrich.net>
2022-04-17 15:40:05 -0400

    Style Nav

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,8 +1,36 @@
 import styled from "styled-components/macro";
 
 const StyledNavigation = styled.nav`
+  width: 90%;
+  max-width: 1440px;
+  padding: 16px;
+  margin: 0 auto;
   display: flex;
-  gap: 10px;
+  align-items: baseline;
+  justify-content: end;
+  gap: 24px;
+
+  a {
+    text-decoration: none;
+    color: #ffffff;
+    font-size: 1.15rem;
+
+    &:visited {
+      color: #ffffff;
+    }
+    &:hover {
+      color: #ffffff;
+      text-decoration: underline;
+    }
+    &:active {
+      color: #ffffff;
+    }
+
+    &.active {
+      font-style: italic;
+      text-decoration: underline;
+    }
+  }
 `;
 
 export default StyledNavigation;
